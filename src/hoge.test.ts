e8f7f317edad8c306d51005fc843bca116d0581f
Casaubon <netzach.yoshida@gmail.com>
2019-03-03 18:09:51 +0900

    fix object test

--- src/hoge.test.ts
+++ src/hoge.test.ts
@@ -4,18 +4,7 @@ test('数値を受け取ってそれが0以上ならtrueになるか', () => {
   expect(isPositive(3)).toBe(true)
 })
 
-class User {
-  name: string
-  age: number
-  private_: boolean
-  constructor(name: string, age: number, private_: boolean) {
-    this.name = name
-    this.age = age
-    this.private_ = private_
-  }
-}
-
-const personA = new User('A', 18, false)
+const personA = {name: 'B', age: 18, private_: false}
 
 test('オブジェクトの型でUserが持てるか', () => {
   expect(showUserInfo(personA)).toBe("OK!")
