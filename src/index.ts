87977cf03e1ae97abce6bc368a854b45558bc3af
Casaubon <netzach.yoshida@gmail.com>
2019-03-03 18:01:11 +0900

    implement object testing

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
 export const isPositive = (num: number):boolean => {
   return num >= 0
 }
+
+interface User {
+  name: string
+  age: number
+  private_: boolean
+}
+
+export const showUserInfo = (user: User):string => {return "OK!"}
