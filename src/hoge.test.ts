17cd0b8f44275d6c4cbec231d68f34db6adc41ad
Casaubon <netzach.yoshida@gmail.com>
2019-03-09 14:18:38 +0900

    数値の配列を受け取って、その0以上の値の和の値を返す関数を実装

--- src/hoge.test.ts
+++ src/hoge.test.ts
@@ -1,4 +1,4 @@
-import { isPositive, showUserInfo } from './index'
+import { isPositive, showUserInfo, sumOfPos } from './index'
 
 test('数値を受け取ってそれが0以上ならtrueになるか', () => {
   expect(isPositive(3)).toBe(true)
@@ -19,4 +19,8 @@ const personA = new User('A', 18, false)
 
 test('オブジェクトの型でUserが持てるか', () => {
   expect(showUserInfo(personA)).toBe("OK!")
+})
+
+test('数値の配列を受け取って、その0以上の値の和の値を返す関数', () => {
+  expect(sumOfPos([1, 3, -2, 0])).toBe(4)
 })
