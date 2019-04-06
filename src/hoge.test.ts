858cce6e84e3d63f6a19c74356e9d564b40abab5
Casaubon <netzach.yoshida@gmail.com>
2019-04-06 17:11:01 +0900

    merge hoge.test.ts

--- src/hoge.test.ts
+++ src/hoge.test.ts
@@ -1,4 +1,4 @@
-import { isPositive, showUserInfo } from './index'
+import { isPositive, showUserInfo, isPositiveFunc } from './index'
 
 test('数値を受け取ってそれが0以上ならtrueになるか', () => {
   expect(isPositive(3)).toBe(true)
@@ -8,4 +8,9 @@ const personA = {name: 'B', age: 18, private_: false}
 
 test('オブジェクトの型でUserが持てるか', () => {
   expect(showUserInfo(personA)).toBe("OK!")
+})
+
+test('数値を受け取ってそれが0以上ならtrue、0未満ならfalse', () => {
+  expect(isPositiveFunc(5)).toBe(true)
+  expect(isPositiveFunc(-1)).toBe(false)
 })
